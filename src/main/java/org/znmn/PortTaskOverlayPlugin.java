318ac8aafea86c0a1bbbbb25f966d04117bab2dd
xzeoss <rob.6@hotmail.com>
2026-01-02 16:51:41 -0500

    Rename & Refactor from example

--- src/main/java/org/znmn/PortTaskOverlayPlugin.java
+++ src/main/java/org/znmn/PortTaskOverlayPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package org.znmn;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Port Task Overlay"
 )
-public class ExamplePlugin extends Plugin
+public class PortTaskOverlayPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private PortTaskOverlayConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	PortTaskOverlayConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(PortTaskOverlayConfig.class);
 	}
 }
--- src/test/java/org/znmn/PortTaskOverlay.java
+++ src/test/java/org/znmn/PortTaskOverlay.java
@@ -1,13 +1,13 @@
-package com.example;
+package org.znmn;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PortTaskOverlay
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(PortTaskOverlayPlugin.class);
 		RuneLite.main(args);
 	}
 }
